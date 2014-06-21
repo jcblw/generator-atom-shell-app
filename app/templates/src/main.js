d602234fa7f774edfccebcbd6a3a4627df76dc43
Ben Anderson <banderson87@gmail.com>
2014-06-21 19:12:40 -0400

    Use latest main.js from quick-start so window doesn't immediately close

--- app/templates/src/main.js
+++ app/templates/src/main.js
@@ -2,22 +2,36 @@
 
 'use strict';
 
-var app = require('app'),
-    BrowserWindow = require('browser-window');
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
-// reports crashes to github
-require('crash-reporter');
+// Report crashes to our server.
+require('crash-reporter').start();
 
-app.on( 'ready', function ( ) {
-    var mainWindow = new BrowserWindow({
-      width : 200,
-      height : 400
-    });
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
 
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-    mainWindow.on( 'closed', function ( ) {
-        mainWindow = null;
-    });
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
 });
 
+// This method will be called when atom-shell has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
